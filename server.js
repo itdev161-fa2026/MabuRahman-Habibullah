83f19ac54cc46d0244caf4b3a843712c071ee706
Mabu Rahman Habibullah <habibulm.gmatc.matc.edu>
2025-09-10 17:47:53 -0500

    Added user registration endpoint

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { json } from 'express';
 import connectDatabase from './config/db.js';
 
 
@@ -7,9 +7,26 @@ const app = express();
 
 connectDatabase();
 
+app.use(express.json({ extended: false }));
+
+// API endpoints
+/**
+ * @route Get /
+ * @desc Test endpoint
+ */
+
 
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route Post api/users
+ * @desc Register user
+ */
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 app.listen(3000, () => console.log('express server running on port 3000'));
