36c157eb5d7e4a05260e9dac8761c4cb7fbbb6e9
Mabu Rahman Habibullah <habibulm.gmatc.matc.edu>
2025-09-10 19:14:27 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express, { json } from 'express';
 import connectDatabase from './config/db.js';
+import {check, validationResult} from 'express-validator';
 
 
 const app = express();
@@ -24,9 +25,19 @@ app.get('/', (req, res) =>
  * @route Post api/users
  * @desc Register user
  */
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post('/api/users', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+], (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    } else {
+        return res.send(req.body);
+    }
+    res.json(req.body);
+}
+);
 
 app.listen(3000, () => console.log('express server running on port 3000'));
